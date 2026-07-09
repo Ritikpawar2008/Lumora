3b711bee05e8178057e2f8f15bb2b7487246664e
Ritik Pawar <santoshritik45@gmail.com>
2026-07-09 09:59:12 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,8 +20,12 @@ app.get("/", (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 5000;
+export default app;
 
-app.listen(PORT, () => {
-    console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+if (process.env.NODE_ENV !== "production") {
+    const PORT = process.env.PORT || 5000;
+
+    app.listen(PORT, () => {
+        console.log(`🚀 Server running on http://localhost:${PORT}`);
+    });
+}
